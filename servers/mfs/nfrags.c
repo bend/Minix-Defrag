79264fc283418fa2b7b458ca500940bbdccfaa41
rbauduin <rbauduin@davidof.info.ucl.ac.be>
2011-05-07 13:22:46 +0200

    nfrags count ok

--- servers/mfs/nfrags.c
+++ servers/mfs/nfrags.c
@@ -8,15 +8,34 @@ PUBLIC int fs_nfrags()
 {
   register int r;              /* return value */
   register struct inode *rip;  /* target inode */
-  int i;
+  off_t pos;
+  int nfrags;
+  block_t block_number, previous_block_number;
 
-  if ((rip = get_inode(fs_dev, (ino_t) fs_m_in.REQ_INODE_NR)) == NULL)
+
+  nfrags=0;
+
+
+  if ((rip = get_inode(fs_dev, (ino_t) fs_m_in.REQ_INODE_NR)) == NULL) /* get inode */
 	return(EINVAL);
   
   printf("FS_NFRAGS_OK\n");
-  for(i=0; i++; i<V2_NR_TZONES){
-	  printf("zone number %d = %u", i, rip->i_zone[i]);
+  for (pos=0; pos<rip->i_size; pos++){                                  /* count number of fragments */
+    block_number = read_map(rip,pos);
+    if (block_number-previous_block_number>1 || block_number-previous_block_number<0 ){
+      printf("currentblock = %d, previous = %d, so we increment nfrags\n", block_number, previous_block_number);
+      nfrags++;
+    }
+    previous_block_number=block_number;
   }
+  printf("number of fragments = %d\n", nfrags);
+
+
+
+
+
+  
+  
   put_inode(rip);		/* release the inode */
   return(r);
 }
