901167187e83ed4ba745e2f85a4c005f1f72fbf6
rbauduin <rbauduin@davidof.info.ucl.ac.be>
2011-05-09 19:31:27 +0200

    first tests of find_region and print_map

--- servers/mfs/defrag.c
+++ servers/mfs/defrag.c
@@ -1,18 +1,192 @@
 #include "fs.h"
+#include "buf.h"
 #include "inode.h"
 #include "super.h"
 #include <minix/vfsif.h>
 #include <stdio.h>
+/*===========================================================================*
+ *				search_free_region				     *
+ *===========================================================================*/
+PUBLIC bit_t search_free_region(sp, map, origin, region_size)
+struct super_block *sp;		/* the filesystem to allocate from */
+int map;			/* IMAP (inode map) or ZMAP (zone map) */
+bit_t origin;			/* number of bit to start searching at */
+{
+/* Allocate a bit from a bit map and return its bit number. */
+
+  block_t start_block;		/* first bit block */
+  block_t block;
+  bit_t map_bits;		/* how many bits are there in the bit map? */
+  short bit_blocks;		/* how many blocks are there in the bit map? */
+  unsigned word, bcount;
+  struct buf *bp;
+  bitchunk_t *wptr, *wlim, k;
+  bit_t i, b;
+  int current_region_size;
+  current_region_size=0;
+
+  if (sp->s_rd_only)
+	panic("can't allocate bit on read-only filesys");
+
+  start_block = START_BLOCK + sp->s_imap_blocks;
+  map_bits = (bit_t) (sp->s_zones - (sp->s_firstdatazone - 1));
+  bit_blocks = sp->s_zmap_blocks;
+  
+
+  /* Figure out where to start the bit search (depends on 'origin'). */
+  if (origin >= map_bits) origin = 0;	/* for robustness */
+
+  /* Locate the starting place. */
+  block = (block_t) (origin / FS_BITS_PER_BLOCK(sp->s_block_size));
+  word = (origin % FS_BITS_PER_BLOCK(sp->s_block_size)) / FS_BITCHUNK_BITS;
+
+  /* Iterate over all blocks plus one, because we start in the middle. */
+  bcount = bit_blocks + 1;
+  do {
+	bp = get_block(sp->s_dev, start_block + block, NORMAL);
+	wlim = &bp->b_bitmap[FS_BITMAP_CHUNKS(sp->s_block_size)];
+
+	/* Iterate over the words in block. */
+	for (wptr = &bp->b_bitmap[word]; wptr < wlim; wptr++) {
+
+		/* Does this word contain a free bit? */
+		if (*wptr == (bitchunk_t) ~0)  {
+            current_region_size=0;
+            continue;
+        }
+
+		/* Increment the free bits. */
+		k = (bitchunk_t) conv2(sp->s_native, (int) *wptr);
+		for (i = 0; i<FS_BITCHUNK_BITS && current_region_size<region_size ; ++i) { 
+            if ( (k & (1 << i)) == 0){
+              current_region_size++;
+              printf("current region size %d\n", current_region_size);
+
+            }
+            else{
+                printf("region ended  \n");
+                current_region_size=0;
+            }
+        }
+
+		/* Bit number from the start of the bit map. */
+		b = ((bit_t) block * FS_BITS_PER_BLOCK(sp->s_block_size))
+		    + (wptr - &bp->b_bitmap[0]) * FS_BITCHUNK_BITS
+		    + i;
+
+		/* Don't allocate bits beyond the end of the map. */
+		if (b >= map_bits) { printf("no region found because beyond end of map")  ;break;}
+
+        b=b-region_size; /* go back to first bit of zone */
+
+
+		/* Allocate and return bit number. */
+        /*
+		k |= 1 << i;
+		*wptr = (bitchunk_t) conv2(sp->s_native, (int) k);
+		bp->b_dirt = DIRTY;
+		put_block(bp, MAP_BLOCK);
+        */
+		return(b);
+	}
+	put_block(bp, MAP_BLOCK);
+	if (++block >= (unsigned int) bit_blocks) /* last block, wrap around */
+		block = 0;
+	word = 0;
+  } while (--bcount > 0);
+  return(NO_BIT);		/* no bit could be allocated */
+}
 
 PUBLIC int fs_defrag()
 {
   register int r;              /* return value */
   register struct inode *rip;  /* target inode */
+  int nblocks;
+  int pos;
+  bit_t first_bit; 
+  block_t block_number, previous_block_number;
+  nblocks = 1;
+  pos = 0;
+
 
   if ((rip = get_inode(fs_dev, (ino_t) fs_m_in.REQ_INODE_NR)) == NULL)
 	return(EINVAL);
-  
-  printf("FS_DEFRAG_OK\n");
+
+  for (pos=0; pos<rip->i_size; pos++){                                  /* count number of fragments */
+    block_number = read_map(rip,pos);
+    if (block_number!=previous_block_number){
+      /* printf("currentblock = %d, previous = %d, so we increment nfrags\n", block_number, previous_block_number);
+      */
+      nblocks++;
+    }
+    previous_block_number=block_number;
+  }
+
+  printf("need %d blocks region", nblocks);
+  first_bit = search_free_region(rip->i_sp, ZMAP,0 ,nblocks); 
+
+  print_map(rip->i_sp, ZMAP);
+  printf("FS_DEFRAG_OK %d\n", first_bit);
   put_inode(rip);		/* release the inode */
   return(r);
 }
+
+
+PUBLIC void print_map(sp, map)
+struct super_block *sp;		/* the filesystem to allocate from */
+int map;			/* IMAP (inode map) or ZMAP (zone map) */
+{
+/* Allocate a bit from a bit map and return its bit number. */
+
+  block_t start_block;		/* first bit block */
+  block_t block;
+  bit_t map_bits;		/* how many bits are there in the bit map? */
+  short bit_blocks;		/* how many blocks are there in the bit map? */
+  unsigned word, bcount;
+  struct buf *bp;
+  bitchunk_t *wptr, *wlim, k;
+  bit_t i, b;
+  int origin;
+  int count_bits;
+  int current_region_size;
+  current_region_size=0;
+  origin=0;
+  start_block = START_BLOCK + sp->s_imap_blocks;
+  map_bits = (bit_t) (sp->s_zones - (sp->s_firstdatazone - 1));
+  bit_blocks = sp->s_zmap_blocks;
+  count_bits=0;
+
+  /* Figure out where to start the bit search (depends on 'origin'). */
+  if (origin >= map_bits) origin = 0;	/* for robustness */
+  /* Locate the starting place. */
+  block = (block_t) (origin / FS_BITS_PER_BLOCK(sp->s_block_size));
+  word = (origin % FS_BITS_PER_BLOCK(sp->s_block_size)) / FS_BITCHUNK_BITS;
+
+  /* Iterate over all blocks plus one, because we start in the middle. */
+  bcount = bit_blocks + 1;
+  do {
+	bp = get_block(sp->s_dev, start_block + block, NORMAL);
+	wlim = &bp->b_bitmap[FS_BITMAP_CHUNKS(sp->s_block_size)];
+
+	/* Iterate over the words in block. */
+	for (wptr = &bp->b_bitmap[word]; wptr < wlim; wptr++) {
+        if(count_bits > map_bits)
+            return;
+  
+		/* Increment the free bits. */
+		k = (bitchunk_t) conv2(sp->s_native, (int) *wptr);
+		for (i = 0; i<FS_BITCHUNK_BITS ; ++i) { 
+            count_bits++;
+            if ( (k & (1 << i)) == 0){
+                printf("_ ");
+            }
+            else{
+                printf("* ");
+            }
+        }
+	}
+	put_block(bp, MAP_BLOCK);
+  } while (--bcount > 0);
+  printf("\n");
+}
+
