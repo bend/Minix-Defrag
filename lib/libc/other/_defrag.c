98d4609b63c0c11eab3d5e46e67d25480b2a7e87
Bend <ben.daccache@gmail.com>
2011-05-15 13:22:50 +0200

    adde return value for defrag

--- lib/libc/other/_defrag.c
+++ lib/libc/other/_defrag.c
@@ -7,9 +7,13 @@
 PUBLIC int defrag(const char* path)
 {
     message m;
+    int nfrags;
     /* CREATE MESSAGE */
     m.m1_i1 = strlen(path) + 1;
     m.m1_p1 = (char* ) path;
-    return (_syscall(VFS_PROC_NR, DEFRAG, &m));
+
+    m.m1_p2 = (char* ) &nfrags;
+    _syscall(VFS_PROC_NR, DEFRAG, &m);
+    return nfrags;
 }
 
